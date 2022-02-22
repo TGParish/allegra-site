95db948085a5edca163138f63b396d4a87d2bc8d
Trever Parish <68043625+TGParish@users.noreply.github.com>
2022-02-22 14:13:53 -0800

    Add files via upload

---
+++ allegra-site/app.js
@@ -0,0 +1,10 @@
+let ouro = document.getElementById("ouro");
+let btn = document.getElementsByClassName("btn");
+
+// ouro.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+
+// btn.addEventListener("click", () => {
+//   console.log("Clicked button");
+// });
